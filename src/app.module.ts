97f2a7d97874218868518d4cada6d8813edd11b8
mynavitechtus-rintc <rintc@mynavitechtus.com>
2026-08-08 14:18:06 +0700

    feat: implement auth module with controller and service scaffolding

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [HealthModule, AuthModule],
   controllers: [],
   providers: [],
 })
