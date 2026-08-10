dac92732976965f8c9d0b17e9f580fce6cc4ff9b
mynavitechtus-rintc <rintc@mynavitechtus.com>
2026-08-10 15:37:26 +0700

    feat: implement users module controller and add findAll service method with unit tests

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -4,4 +4,8 @@ import { PrismaService } from 'src/providers/prisma/prisma.service';
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
 }
